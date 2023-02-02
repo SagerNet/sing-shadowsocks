e3123545f2f768b22623aac44bfae548d8e0e9a2
世界 <i@sekai.icu>
2023-02-02 11:50:33 +0800

    Fix crypto/cipher.xorWords removed

--- shadowaead_2022/xor.go
+++ shadowaead_2022/xor.go
@@ -1,7 +1,7 @@
+//go:build go1.20
+
 package shadowaead_2022
 
-import _ "unsafe"
+import "crypto/subtle"
 
-//go:linkname xorWords crypto/cipher.xorWords
-//go:noescape
-func xorWords(dst, a, b []byte)
+var xorWords = subtle.XORBytes
