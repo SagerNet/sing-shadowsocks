1a7c32b4e2e73235d99fe674ec82b58d43b68668
世界 <i@sekai.icu>
2023-04-12 20:31:10 +0800

    Fix ReadFrom result

--- shadowaead_2022/protocol.go
+++ shadowaead_2022/protocol.go
@@ -702,7 +702,11 @@ func (c *clientPacketConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) 
 	if err != nil {
 		return
 	}
-	addr = destination.UDPAddr()
+	if destination.IsFqdn() {
+		addr = destination
+	} else {
+		addr = destination.UDPAddr()
+	}
 	n = copy(p, buffer.Bytes())
 	return
 }
